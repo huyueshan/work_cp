7228dc76f0b1acae6f6418b13b5408737da91deb
zhangguoyu <781630256@qq.com>
2018-05-22 18:12:55 +0800

    first commit

---
+++ src/status/language.ts
@@ -0,0 +1,13 @@
+const language_0 = {
+	'en':{
+		'ENGLISH':'english',
+		'CHINESE':'chinese',
+		'CHANGE_LANG':'change language'
+	},
+	'zh':{
+		'ENGLISH':'英文',
+		'CHINESE':'中文',
+		'CHANGE_LANG':'改变语言'
+	}
+}
+export default language_0;
