29490e814635f7149c0517c37eba33611f9b81ef
chenzg <450418326@qq.com>
2018-05-29 14:27:25 +0800

    投注记录提交

--- src/app/page_module_1/usercenter/usercenter.component.ts
+++ src/app/page_module_1/usercenter/usercenter.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { SharkModule } from '@ntesmail/shark-angular2';
 import { Router, ActivatedRoute, Params } from "@angular/router";
 
+import { GOUC, userdef } from '../../../factory/usercent';
+
 @Component({
   selector: 'app-usercenter',
   templateUrl: './usercenter.component.html',
@@ -16,7 +18,6 @@ export class UsercenterComponent implements OnInit {
       title:'投注记录',
       isover: false,
       isactive:false,
-      // itemactivenmb:-1,
       items:[
         {
           text:'购彩查询',
@@ -29,26 +30,78 @@ export class UsercenterComponent implements OnInit {
       ]
     },
     {
-      title:'投注记录1',
+      title:'报表管理',
       isover: false,
       isactive:false,
-      // itemactivenmb:-1,
       items:[
         {
-          text:'购彩查询1',
-          link:''
+          text:'充值记录',
+          link:'/usercenter/goucaiquery',
         },
         {
-          text:'追号查询1',
-          link:''
+          text:'帐变报表',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'个人报表',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'优惠活动详情',
+          link:'/usercenter/Zhuihaoquery',
         }
       ]
     },
     {
-      title:'投注记录2',
+      title:'账户管理',
       isover: false,
       isactive:false,
-      // itemactivenmb:-1,
+      items:[
+        {
+          text:'个人总览',
+          link:'/usercenter/goucaiquery',
+        },
+        {
+          text:'安全中心',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'银行卡管理',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'用户资料',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'彩种信息',
+          link:'/usercenter/Zhuihaoquery',
+        },
+        {
+          text:'彩种限额',
+          link:'/usercenter/Zhuihaoquery',
+        },
+      ]
+    },
+    {
+      title:'代理管理',
+      isover: false,
+      isactive:false,
+      items:[
+        {
+          text:'购彩查询2',
+          link:'',
+        },
+        {
+          text:'追号查询2',
+          link:'',
+        }
+      ]
+    },
+    {
+      title:'短信公告',
+      isover: false,
+      isactive:false,
       items:[
         {
           text:'购彩查询2',
@@ -67,23 +120,23 @@ export class UsercenterComponent implements OnInit {
 
   ngOnInit() {
   }
+  // }
+
+  // 左侧导航栏一级菜单点击事件
   itemboxclick(i){
     if (this.currentactive == i) {
       this.currentactive = -1;
-      
     }else{
-
       this.currentactive = i;
     }
-    // let act = this.usersidedata[i];
-    // act.isactive = !act.isactive;
-    // if(!act.isactive){
-    //   act.itemactivenmb = -1;
-    // }
   }
+  // 左侧导航栏二级菜单点击事件
   itemclick(f,t,L){
+    // 当前选中用户中心一级菜单
     this.currentfristitem = f;
+    // 当前选中用户中心二级菜单
     this.currentitem = t;
+    // 跳转路由
     this.router.navigate([L]);
   }
 
--- src/factory/usercent.ts
+++ src/factory/usercent.ts
@@ -1,29 +1,33 @@
 export class GOUC {
-    id: number;  //注单编号
-    time: string; //投注时间
-    user: string; //用户
-    style: string; //彩种
-    playtype: string; //玩法
-    content: string; //投注内容
-    issue: string; //投注期号
-    money: number; //投注金额
-    rebates: number; //投注返点
-    bonus: number; //奖金
-    currentnumber: string; //开奖号码
-    state: boolean; //中奖状态
-  }
+  id: {number,string}; //注单编号
+  time: string; //投注时间
+  user: string; //用户
+  style: string; //彩种
+  playtype: string; //玩法
+  content: string; //投注内容
+  issue: string; //投注期号
+  money: {number,string}; //投注金额
+  rebates: {number,string}; //投注返点
+  bonus: {number,string}; //奖金
+  currentnumber: string; //开奖号码
+  state: {boolean,string}; //中奖状态
+}
 
-  export const Goucdef =  {
-    id: 0,  //注单编号
-    time: '2018-08-08', //投注时间
-    user: 'test02', //用户
-    style: 'all', //彩种
-    playtype: 'all', //玩法
-    content: '0,0,0,0,0', //投注内容
-    issue: '088', //投注期号
-    money: 0, //投注金额
-    rebates: 0, //投注返点
-    bonus: 0, //奖金
-    currentnumber: '0,0,0,0,0', //开奖号码
-    state: false, //中奖状态
-  }
+const Goucdef = {
+  id: '0', 
+  time: "2018-08-08",
+  user: "test02", 
+  style: "all", 
+  playtype: "all",
+  content: "0,0,0,0,0", 
+  issue: "088",
+  money: 0, 
+  rebates: 0,
+  bonus: 0, 
+  currentnumber: "0,0,0,0,0",
+  state: false 
+};
+
+export const userdef = {
+  Goucdef
+};
